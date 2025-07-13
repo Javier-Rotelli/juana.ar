61af0cbd85355ff5310db068da9ea0ad2d86ff1d
Javier <javier.rotelli@gmail.com>
2025-07-13 02:18:33 -0300

    form andando

--- src/SixthPage.jsx
+++ src/SixthPage.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import Page from "./Page";
+import { useFormStatus } from "react-dom";
 
 const WEB_APP_URL =
   "https://script.google.com/macros/s/AKfycbxo_uShAdsTnZiVVeFZBsUm4IvkP9oG_2oiTLJqFEOaN4aKIlG0S14cgb34nL_ibO9SAg/exec";
 
-function RSVPForm() {
+function Submit() {
+  const status = useFormStatus();
+  return <button disabled={status.pending}>Submit</button>;
+}
+
+function RSVPForm({ onSubmited }) {
+  const [error, setError] = useState();
+  const [pending, setPending] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
+    setPending(true);
     const formData = new FormData(event.target);
 
     fetch(WEB_APP_URL, {
@@ -15,8 +26,14 @@ function RSVPForm() {
       if (!response.ok) {
         console.error("Network response was not ok");
         console.error(response);
-        throw new Error("Network response was not ok");
+        setError(
+          "Hubo un error al enviar el formulario. Por favor, intenta nuevamente.",
+        );
+      } else {
+        setError("");
+        onSubmited(true);
       }
+      setPending(false);
     });
   };
 
@@ -28,24 +45,60 @@ function RSVPForm() {
       className="rsvp-form"
     >
       <h1>Los esperamos!</h1>
+      {error && <p className="error">{error}</p>}
       <label htmlFor="Email">Email:</label>
-      <input name="Email" type="email" required />
-      <label htmlFor="Asistentes">Asistentes:</label>
-      <input name="Asistentes" type="text" required />
+      <input
+        name="Email"
+        type="email"
+        required
+        onInvalid={(e) => {
+          e.target.setCustomValidity("Por favor, ingresa un email válido.");
+        }}
+        onInput={(e) => {
+          e.target.setCustomValidity("");
+        }}
+      />
+      <label htmlFor="Asistentes">
+        Asistentes (indicar nombre y apellido):
+      </label>
+      <input
+        name="Asistentes"
+        type="text"
+        required
+        onInvalid={(e) => {
+          e.target.setCustomValidity(
+            "Por favor, ingresa al menos un asistente.",
+          );
+        }}
+        onInput={(e) => {
+          e.target.setCustomValidity("");
+        }}
+      />
       <label htmlFor="Restricciones">Restricciones alimentarias:</label>
       <input name="Restricciones" type="text" />
       <label htmlFor="cancion">Mi canción:</label>
       <input name="cancion" type="text" />
-      <button type="submit">CONFIRMAR ASISTENCIA</button>
+      <button disabled={pending} type="submit">
+        {!pending ? "CONFIRMAR ASISTENCIA" : "ENVIANDO..."}
+      </button>
     </form>
   );
 }
 
 function SixthPage() {
+  const [submitted, setSubmitted] = useState(false);
+
   return (
     <Page className="sixth-page">
       <img src="public/Image 4.png" />
-      <RSVPForm />
+      {!submitted ? (
+        <RSVPForm onSubmited={setSubmitted} />
+      ) : (
+        <div className="rsvp-success">
+          {/* TODO: copy para esto */}
+          <h1>Gracias por confirmar tu asistencia!</h1>
+        </div>
+      )}
     </Page>
   );
 }
